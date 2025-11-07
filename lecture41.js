fb88acb5cceee7741380376c30f207389bb91508
technodivs <devanshi.23b1531037@abes.ac.in>
2025-11-07 10:26:26 +0530

    Add files via upload

---
+++ lecture41.js
@@ -0,0 +1,73 @@
+// console.log("hello world");
+let a = 10;
+ let b =5;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a*b);
+// console.log(++a);
+// console.log(a++);
+// console.log(a--);
+// console.log(--a);
+// a=a+10;
+// console.log(a);
+// a += 10;
+// console.log(a);
+// console.log(10>5);
+// console.log(10<5);
+// console.log('5'== 5);
+// console.log('5'=== 5);
+// console.log('5'!= 5);
+// console.log('5'!== 5);
+// let age =34;
+// let status1= (age>18)? 'adult':'not adult';
+// console.log(status1);
+// age=5;
+// if (age>18) {
+//     console.log("can vote");
+// }
+// else{
+//     console.log("cannot vote");
+// }
+// let number=7;
+// if(number==1){
+//     console.log("one");
+// }
+// else if(number==2){
+//     console.log("two");
+// }
+// else if(number==3){
+//     console.log("three");
+// }
+// else if(number==4){
+//     console.log("four");
+// }
+// else if(number==5){
+//     console.log("five");
+// }
+// else{
+//     console.log("number is not between 1 to 5");
+// }
+
+let num=3;
+switch(num){
+    case 1:
+        console.log("one");
+        break;
+    case 2:
+        console.log("two");
+        break;
+    case 3:
+        console.log("three");
+        break;
+    case 4:
+        console.log("four");
+        break;
+    case 5:
+        console.log("five");
+        break;
+    default:
+        console.log("number is not between 1 to 5");
+}
+
